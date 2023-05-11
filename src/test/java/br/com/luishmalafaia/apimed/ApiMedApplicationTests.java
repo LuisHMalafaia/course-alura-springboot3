1c452aa545a8d3bf043cfdb77b6bf2a9b8e9ca2e
Luis Henrique Malafaia <luishmalafaia@gmail.com>
2023-05-10 21:37:49 -0300

    initial commit

---
+++ src/test/java/br/com/luishmalafaia/apimed/ApiMedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luishmalafaia.apimed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
